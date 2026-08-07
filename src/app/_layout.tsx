11096793fe2fe8bb7b497dfc1dde66afa6fdefa4
sosabbo <sophia.sabbo@gmail.com.br>
2026-08-07 12:18:23 -0300

    feat: Adicionando react query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,9 +2,12 @@ import { Stack } from "expo-router";
 import { useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../styles/global.css";
 
 SplashScreen.preventAutoHideAsync();
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
@@ -21,5 +24,9 @@ export default function RootLayout() {
     return null;
   }
 
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }
